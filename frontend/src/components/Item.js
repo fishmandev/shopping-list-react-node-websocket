fb542ffe164ce0dc54c8e1b01ae3c74ec01c56f7
Dmitriy Fishman <fishman.code@gmail.com>
2021-06-10 18:31:28 +0300

    Added handlers of an item

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,16 +1,23 @@
 import { deleteButton } from "./icons";
 import "./Item.css"
 
-const Item = ({ item }) => {
+const Item = ({ item, onChangeBuyStatus, onDelete }) => {
 
   return (
     <div className='Item'>
       <input type="checkbox"
         className='Done'
         checked={item.isBought}
+        data-id={item.id}
+        onChange={onChangeBuyStatus}
       />
       <p className={`Item-Text ${item.isBought ? "Bought" : ""}`}>{item.name}</p>
-      <button className='Item-Delete'>{deleteButton}</button>
+      <button
+        className='Item-Delete'
+        onClick={() => onDelete(item.id)}
+      >
+        {deleteButton}
+      </button>
     </div>
   );
 };
