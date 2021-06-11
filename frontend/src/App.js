2304b8030ee816977bfa337ccde397f5e0ceca3f
Dmitriy Fishman <fishman.code@gmail.com>
2021-06-11 12:39:40 +0300

    Added all components to the App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
+import useList from './api/useList';
+import CreateItemInput from './components/CreateItemInput';
+import List from './components/List';
 import './App.css';
 
 function App() {
   const [list, setList] = useState([]);
-  
-  useEffect(() => {  
+  const [item, setItem] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+  const api = useList();
+
+  useEffect(() => {
     const wsConnect = () => {
       let webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_SERVER_URL);
       webSocket.onclose = () => {
@@ -12,22 +18,55 @@ function App() {
       }
       webSocket.onmessage = event => {
         setList(JSON.parse(event.data));
+        setIsLoaded(false);
       }
     }
     const fetchList = () => {
-      fetch(process.env.REACT_APP_API_SERVER_URL + '/lists')
-        .then(response => response.json())
-        .then(data => {
-          setList(data.items);
-        })
+      setIsLoaded(true);
+      api.read().then(setList).finally(() => { setIsLoaded(false) });
     }
     wsConnect();
     fetchList();
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const onChangeBuyStatus = ({ target: { checked, dataset: { id } } }) => {
+    setIsLoaded(true);
+    api.update([id], checked).finally(() => { setIsLoaded(false) });
+  };
+
+  const onDelete = (id) => {
+    setIsLoaded(true);
+    api.remove(id).finally(() => { setIsLoaded(false) });
+  };
+
+  const onCreateItemChange = (value) => setItem(value);
+
+  const onCreateItem = () => {
+    if (!item) {
+      return; // Not valid
+    }
+    setIsLoaded(true);
+    api.create(item).then(() => {
+      setItem('');
+    }).finally(() => { setIsLoaded(false) });
+  };
+
   return (
     <div className="App">
-      <h3>Hello React!</h3>
+      <div>
+        <CreateItemInput
+          value={item}
+          onCreateItemChange={onCreateItemChange}
+          onCreateItem={onCreateItem}
+        />
+        <List
+          list={list}
+          onChangeBuyStatus={onChangeBuyStatus}
+          onDelete={onDelete}
+        />
+      </div>
+      { isLoaded && <div className="Loader"></div>}
     </div>
   );
 }
