9b0d65b6c3c7171fb8421b58033787fbb2e58653
Dmitriy Fishman <fishman.code@gmail.com>
2021-06-11 09:31:35 +0300

    Added an API client hook

---
+++ frontend/src/api/useClient.js
@@ -0,0 +1,31 @@
+const useClient = () => {
+  const fetch = (endpoint, { body, ...customConfig } = {}) => {
+    const apiUrl = process.env.REACT_APP_API_SERVER_URL || 'http://localhost';
+    const headers = { 'Content-Type': 'application/json' };
+    const config = {
+      method: body ? 'POST' : 'GET',
+      ...customConfig,
+      headers: {
+        ...headers,
+        ...customConfig.headers,
+      },
+    };
+    if (body) {
+      config.body = JSON.stringify(body)
+    }
+
+    return window
+      .fetch(`${apiUrl}/${endpoint}`, config)
+      .then(response => {
+        if (response.ok) {
+          if (response.status === 200 || response.status === 201)
+            return response.json();
+          return '';
+        }
+      });
+  };
+
+  return { fetch };
+}
+
+export default useClient;
