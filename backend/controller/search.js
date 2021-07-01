ca9b1cf26ecd90255bf1299f299973c97128a9f7
Dmitriy Fishman <fishman.code@gmail.com>
2021-07-01 02:20:52 -0400

    Makes searche, if the query contains at least one character

--- backend/controller/search.js
+++ backend/controller/search.js
@@ -2,8 +2,12 @@ const product = require('../model/product');
 
 module.exports = {
   query: (req, res, next) => {
-    product.search(req.body.query).then(rows => {
-      return res.json({ 'items': rows });
-    }).catch(next);
+    if (req.body.query.length > 0) {
+      product.search(req.body.query).then(rows => {
+        return res.json({ 'items': rows });
+      }).catch(next);
+    } else {
+      return res.json({ 'items': [] });
+    }
   }
 };
