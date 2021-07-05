a8d4e9dd03117d3eaa2c5c73be5acaba2bc68c60
Dmitriy Fishman <fishman.code@gmail.com>
2021-07-05 00:01:38 -0400

    Implementing autocomplete component

--- frontend/src/components/Autocomplete.js
+++ frontend/src/components/Autocomplete.js
@@ -1,8 +1,59 @@
+import { useState, useRef } from 'react';
+import useProduct from './../api/useProduct';
+import './Autocomplete.css';
 
-const Autocomplete = () => {
+const Autocomplete = ({ value, onCreateItemChange, onCreateItem }) => {
+  const searchInput = useRef(null);
+  const [suggestions, setSuggestions] = useState([]);
+  const product = useProduct();
+
+  const onChangeHandler = (query) => {
+    onCreateItemChange(query);
+    if (query.length > 0) {
+      product.search(query).then((data) => {
+        setSuggestions(data.items);
+      });
+    } else {
+      setSuggestions([]);
+    }
+  }
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter')
+      onCreateItem();
+  }
+
+  const onBlurHandler = () => {
+    setTimeout(() => {
+      setSuggestions([]);
+    }, 200);
+  }
+
+  const onChooseSuggestion = (text) => {
+    onCreateItemChange(text);
+    searchInput.current.focus();
+  }
 
   return (
-    <div></div>
+    <div className="Input">
+      <input
+        ref={searchInput}
+        type="text"
+        onChange={e => onChangeHandler(e.target.value)}
+        onBlur={onBlurHandler}
+        onKeyPress={onKeyPress}
+        value={value}
+      />
+      {suggestions && suggestions.map((item) =>
+        <div
+          className="Suggestion"
+          key={item.id}
+          onClick={() => onChooseSuggestion(item.name)}
+        >
+          {item.name}
+        </div>
+      )}
+    </div>
   );
 }
 
